bdd1b53bb987a261c56500583307b1569d45d41e
Hany Antar <158524457+hanyanter@users.noreply.github.com>
2024-03-31 11:16:51 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Hospital Management sys.",
+    'author': "Hany Antar Hassan",
+    'category': "",
+    'version': "17.0.0.1.0",
+    'description': """This module made to save the main
+    information about doctors and patients""",
+    'depends': ['base',
+                ],
+    'data': [
+        'views/base_menu.xml',
+        'views/doctor_view.xml',
+    ],
+    'assets': {},
+    'application': True
+}
